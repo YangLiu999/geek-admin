4b3726d1cbc86f5d792f992f45e1a574e3847fa3
yangliu <15998990247@163.com>
2024-05-22 16:08:34 +0800

    Sass 作为 CSS 预处理语言

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -63,8 +63,23 @@
     
 </script>
 
-<style>
-    /* h1 {
-        color: red;
-    } */
+<!-- npm install -D sass -->
+<style lang="scss" scoped>
+$padding:10px;
+$white:#fff;
+ul {
+  width:500px;
+  margin:0 auto;
+  padding: 0;
+  li {
+    &:hover {
+      cursor: pointer;
+    }
+    list-style-type: none;
+    margin-bottom: $padding;
+    padding: $padding;
+    background: $white;
+    box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+  }
+}
 </style>
